41e6cdac4dcedeba38802300f617c0f3f2026bfc
Pavlo Sheremet <pashqa.sheremet@gmail.com>
2024-12-29 17:33:20 +0200

    save task 5

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,18 +1,23 @@
-import TodoListItem from "../TodoListItem/TodoListItem";
+import TodoListItem from '../TodoListItem/TodoListItem';
 import Grid from '../Grid/Grid';
 import GridItem from '../GridItem/GridItem';
 
-const TodoList = ({todos, deleteTodo}) => {
+const TodoList = ({ todos, deleteTodo, openEditForm }) => {
   return (
-  <Grid>
-  {todos.map((item, index) => (
-    <GridItem key={item.id} >
-      
-      <TodoListItem id={item.id} text={item.text} index={index +1} deleteTodo={deleteTodo}/>
-    </GridItem>
-  ))}
-</Grid>
-  )
+    <Grid>
+      {todos.map((item, index) => (
+        <GridItem key={item.id}>
+          <TodoListItem
+            id={item.id}
+            text={item.text}
+            index={index + 1}
+            deleteTodo={deleteTodo}
+            openEditForm={openEditForm}
+          />
+        </GridItem>
+      ))}
+    </Grid>
+  );
 };
-  
+
 export default TodoList;
--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -2,15 +2,27 @@ import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 import style from './TodoListItem.module.css';
 import Text from '../Text/Text';
 
-const TodoListItem = ({ text, index, id, deleteTodo }) => {
+const TodoListItem = ({ text, index, id, deleteTodo, openEditForm }) => {
   return (
     <div className={style.box}>
       <Text textAlign="center" marginBottom="20">
         TODO #{index}
       </Text>
       <Text>{text}</Text>
-      <button onClick={() => deleteTodo(id)} className={style.deleteButton} type="button">
+      <button
+        onClick={() => deleteTodo(id)}
+        className={style.deleteButton}
+        type="button"
+      >
         <RiDeleteBinLine size={24} />
+      </button>
+
+      <button
+        onClick={() => openEditForm({ text, id })}
+        className={style.editButton}
+        type="button"
+      >
+        <RiEdit2Line size={24} />
       </button>
     </div>
   );
